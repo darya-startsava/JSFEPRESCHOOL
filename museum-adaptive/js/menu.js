a06089418abe209aa2c5e0f0b43e48dd696420c5
darya-startsava <daryastartsava@gmail.com>
2021-10-05 01:48:55 +0200

    feat: add burger menu

---
+++ museum-adaptive/js/menu.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const menuItem = document.querySelectorAll(".menu-item");
+const welcomeTitle = document.querySelector(".welcome-title");
+
+hamburger.addEventListener("click", openHamburgerMenu);
+menuItem.forEach(n => n.addEventListener("click", closeMenu));
+
+function openHamburgerMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    welcomeTitle.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    welcomeTitle.classList.toggle("active");
+}
